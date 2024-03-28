5e3f158ba9aba076ea150b33fd7793cb3c5ab249
CptnBluebrd <laurens_beulink@hotmail.nl>
2024-03-28 17:01:11 +0100

    Push

--- django_mindforest/django_mindforest/settings.py
+++ django_mindforest/django_mindforest/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_ROOT =  BASE_DIR.joinpath('media')
+MEDIA_URL = "/media/"
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
