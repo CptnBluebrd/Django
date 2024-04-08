d92ed7b295028653c0667ae57b68c64c2ba42ea6
CptnBluebrd <laurens_beulink@hotmail.nl>
2024-04-08 10:57:12 +0200

    Pull

--- django_mindforest/articles/urls.py
+++ django_mindforest/articles/urls.py
@@ -4,7 +4,8 @@ from .views import (
     PostDetailView, 
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView
 )
 from . import views
 
@@ -12,9 +13,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='articles-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', PostListView.as_view(), name='about-me'),
+    
 ]
